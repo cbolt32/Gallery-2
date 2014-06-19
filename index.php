54c58c7db82320915f103f304179e76e51e04a2f
cbolt32 <cbolt32@yahoo.co.uk>
2014-06-19 19:50:14 +0100

    Almost complete

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html id="landing" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Shannon Sait | Fine Artist, Welcome To My Site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/myStyles.css" rel="stylesheet">
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myScript.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <![endif]-->
+
+</head>
+
+<body id="landingPage">
+
+<?php include "_/components/php/article-landingPage.php"; ?>
+
+</body>
+<!--</section>-->
+<!--</body>-->
+</html>
